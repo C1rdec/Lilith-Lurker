c405235e4a388c846c8cf1e0961d79424f6d0169
C1rdec <cedric_lampron@hotmail.com>
2023-06-15 20:32:32 -0400

    Apply scalling in margin converter

--- LilithLurker/Converters/MarginConverter.cs
+++ LilithLurker/Converters/MarginConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 using LilithLurker.Models;
@@ -10,9 +11,14 @@ namespace LilithLurker.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            var window = Application.Current.Windows.OfType<Window>().SingleOrDefault(x => x.IsActive);
+            var source = PresentationSource.FromVisual(window);
+
+            var scaleX = source.CompositionTarget.TransformToDevice.M11;
+            var scaleY = source.CompositionTarget.TransformToDevice.M22;
             if (value is Position position)
             {
-                return new Thickness(position.Left, position.Top, 0, 0);
+                return new Thickness(position.Left / scaleX, position.Top / scaleY, 0, 0);
             }
 
             return null;
